50e3a53ce980e8f0bd77cbd332dcd440899ab59c
Thamir K. AlHashemi <thamir711@yahoo.com>
2015-12-27 23:04:28 +0300

    Added code to explore correlation between sectors  in R_Lab_2_5.R

--- R Lab 2_5.R
+++ R Lab 2_5.R
@@ -1,23 +1,74 @@
 library(quantmod)
+library(ellipse)
 
 dataPath <- "~/R/BayanAnalytics/data/"
-startDate <- as.Date("2000-01-01")
+startDate <- as.Date("2007-01-06")
 
-symbol <- "TASI"
-TASI <- read.csv(paste(dataPath, symbol, ".csv", sep=""))
-TASI <- xts(TASI[, c("Open","High","Low","Close","Volume")], order.by=as.Date(TASI[, "Index"], format="%Y-%m-%d"))
-TASI <- window(TASI, start=startDate)
+symbols <- c("TASI",
+             "TASI.BFS",
+             "TASI.PCI",
+             "TASI.CMT",
+             "TASI.RTL",
+             "TASI.EU",
+             "TASI.AFI",
+             "TASI.TIT",
+             "TASI.INS",
+             "TASI.MUI",
+             "TASI.INI",
+             "TASI.BDC",
+             "TASI.RED",
+             "TASI.TRA",
+             "TASI.MAP",
+             "TASI.HTT")
 
-symbol <- "TASI.BFS"
-TASI.BFS <- read.csv(paste(dataPath, symbol, ".csv", sep=""))
-TASI.BFS <- xts(TASI.BFS[, c("Open","High","Low","Close","Volume")], order.by=as.Date(TASI.BFS[, "Index"], format="%Y-%m-%d"))
-TASI.BFS <- window(TASI.BFS, start=startDate)
+returns <- data.frame()
 
-TASI_returns <- Delt(Cl(TASI), type="arithmetic")
-TASI_logReturns <- Delt(Cl(TASI), type="log")
+for (symbol in symbols) {
+  data <- read.csv(paste(dataPath, symbol, ".csv", sep=""))
+  data <- xts(data[, c("Open","High","Low","Close","Volume")], order.by=as.Date(data[, "Index"], format="%Y-%m-%d"))
+  data <- window(data, start=startDate)
+  
+  print(paste(symbol, ": ", length(data), " rows", sep=""))
+  returns <- cbind(returns, Delt(Cl(data), type="log"))
+}
 
-TASI.BFS_returns <- Delt(Cl(TASI.BFS), type="arithmetic")
-TASI.BFS_logReturns <- Delt(Cl(TASI.BFS), type="log")
+colnames(returns) <- symbols
+returns <- returns[-1, ]
 
-plot(as.numeric(TASI_returns[-1, ]), as.numeric(TASI.BFS_returns[-1, ]))
-cor(as.numeric(TASI_returns[-1, ]), as.numeric(TASI.BFS_returns[-1, ]))
+cor(returns)
+symnum(cor(returns))
+
+# Exclude TASI All Index from the correlation table.
+# Plot the correlation matrix.
+ctab <- cor(returns[, -1])
+ctab <- round(ctab, 2)
+plotcorr(ctab, numbers=TRUE, type="lower", diag=TRUE, mar=c(0.1, 0.1, 0.1, 0.1), cex.lab=0.75)
+
+# Finding the sectors with the strongest correlation.
+# To use the max() function, we need to remove the correlation between a sector and itself (i.e. correlation = 1)
+ctab[ctab == 1] <- NA
+max(ctab, na.rm=TRUE) # the max correlation is found to be 0.85
+table(ctab == 0.85)
+ctab == 0.85 # check visually, the strongest correlation is found to be beween TASI.INI and TASI.BDC
+
+# TASI.INI and TASI.BDC dialy log returns have the highest positive correlation.
+cor(returns[, "TASI.INI"], returns[, "TASI.BDC"])
+plot(as.numeric(returns[, "TASI.INI"]), as.numeric(returns[, "TASI.BDC"]), xlab="TASI.INI", ylab="TASI.BDC")
+
+# Finding the sectors with the weakest correlation.
+min(ctab, na.rm=TRUE) # the max correlation is found to be 0.85
+table(ctab == 0.35)
+ctab == 0.35 # check visually, the weakest correlation is found to be beween TASI.EU and TASI.MAP
+
+# TASI.EU and TASI.MAP dialy log returns have the lowest positive correlation.
+cor(returns[, "TASI.EU"], returns[, "TASI.MAP"])
+plot(as.numeric(returns[, "TASI.EU"]), as.numeric(returns[, "TASI.MAP"]), xlab="TASI.EU", ylab="TASI.MAP")
+
+
+# TASI and TASI.PCI returns have the highest positive correlation.
+cor(returns[, "TASI"], returns[, "TASI.PCI"])
+plot(as.numeric(returns[, "TASI"]), as.numeric(returns[, "TASI.PCI"]))
+
+# TASI and TASI.MAP returns have the lowest positive correlation.
+cor(returns[, "TASI"], returns[, "TASI.MAP"])
+plot(as.numeric(returns[, "TASI"]), as.numeric(returns[, "TASI.MAP"]))
--- data/code/ListOfInstruments.R
+++ data/code/ListOfInstruments.R
@@ -1,10 +1,42 @@
 ##########################################
 ## List of securities (GulfBase tickers) 
 ##########################################
-theInstruments <- c("TASI",
-                    "TASI.BFS",
-                    "NCB",
-                    "SABIC",
-                    "JABALOMAR",
-                    "SECO",
-                    "STC")
+# Sector symbols:
+# TASI.BFS - Banks & Financial Services
+# TASI.PCI - Petrochemcial Industries
+# TASI.CMT - Cement
+# TASI.RTL - Retail
+# TASI.EU  - Energy & Utilities
+# TASI.AFI - Agriculture & Food Industries
+# TASI.TIT - Telecommunication & Information Technology
+# TASI.INS - Insurance
+# TASI.MUI - Multi-Investment
+# TASI.INI - Industrial Investment
+# TASI.BDC - Building & Construction
+# TASI.RED - Real Estate Development
+# TASI.TRA - Transport
+# TASI.MAP - Media & Publishing
+# TASI.HTT - Hotel & Tourism
+
+#theInstruments <- c("TASI",
+#                    "TASI.BFS",
+#                    "NCB",
+#                    "SABIC",
+#                    "JABALOMAR",
+#                    "SECO",
+#                    "STC")
+
+theInstruments <- c("TASI.PCI",
+                    "TASI.CMT",
+                    "TASI.RTL",
+                    "TASI.EU",
+                    "TASI.AFI",
+                    "TASI.TIT",
+                    "TASI.INS",
+                    "TASI.MUI",
+                    "TASI.INI",
+                    "TASI.BDC",
+                    "TASI.RED",
+                    "TASI.TRA",
+                    "TASI.MAP",
+                    "TASI.HTT")
